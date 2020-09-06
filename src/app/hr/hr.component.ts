987c603684b6dbb4fc6778733582677f6ade2504
Vivekanand P V <vivekanandpv@outlook.com>
2020-09-06 16:03:31 +0530

    query params

--- src/app/hr/hr.component.ts
+++ src/app/hr/hr.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-hr',
   templateUrl: './hr.component.html',
-  styleUrls: ['./hr.component.scss']
+  styleUrls: ['./hr.component.scss'],
 })
 export class HrComponent implements OnInit {
+  city: string;
+  year: number;
+  qp: object;
 
-  constructor() { }
+  constructor(private ar: ActivatedRoute) {}
 
   ngOnInit(): void {
-  }
+    this.city = this.ar.snapshot.paramMap.get('city');
+    this.year = +this.ar.snapshot.paramMap.get('year');
 
+    this.qp = this.ar.snapshot.queryParams;
+  }
 }
