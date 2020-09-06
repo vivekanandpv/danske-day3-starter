85121e7e1612e22697d0ba387890a6c5beafe976
Vivekanand P V <vivekanandpv@outlook.com>
2020-09-06 15:44:02 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FinanceComponent } from './finance/finance.component';
+import { DevComponent } from './dev/dev.component';
+import { HrComponent } from './hr/hr.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'finance',
+    component: FinanceComponent,
+  },
+  {
+    path: 'dev',
+    component: DevComponent,
+  },
+  {
+    path: 'hr',
+    component: HrComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PlaygroundComponent } from './playground/playground.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DevComponent } from './dev/dev.component';
+import { FinanceComponent } from './finance/finance.component';
+import { HrComponent } from './hr/hr.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PlaygroundComponent
+    PlaygroundComponent,
+    HomeComponent,
+    AboutComponent,
+    DevComponent,
+    FinanceComponent,
+    HrComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
