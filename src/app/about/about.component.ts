74365026f1426e683923c202cf33714adabbe9ab
Vivekanand P V <vivekanandpv@outlook.com>
2020-09-12 09:12:49 +0530

    programmatic navigation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  clickHandler() {
+    // programmatic navigation
+    this.router.navigate(['/hr', 'Madurai', 2005], {
+      fragment: 'my-fragment',
+      queryParams: {
+        country: 'India',
+        state: 'Tamil Nadu',
+        'reach-by': 'Air',
+      },
+    });
   }
-
 }
